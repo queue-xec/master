c30ae86a06a1e8fb97c09e33fd7ea17a53d8611e
KosM <mpoukouvalas.kk@gmail.com>
2022-09-25 23:18:39 +0300

    test: added jest and some tests

---
+++ __tests__/index.spec.js
@@ -0,0 +1,61 @@
+const mockeData = require('./utils');
+const Master = require('../index');
+
+process.env.transferEncryptToken = '00000000000000000000000000000000';
+process.env.token = 'demoCHannel0';
+process.env.LOG_LEVEL = 'off';
+
+let master;
+beforeEach(() => {
+    master = new Master();
+});
+
+describe('pushNewJob method', () => {
+    it('Reject push job if not pass payload', async () => {
+        await master.pushNewJob().catch((e) => {
+            expect(e.message).toBe('payload is undefined');
+        });
+    });
+    it('Push job succesfully and enrcypted , if job is object', async () => {
+        const encryptedPayload = master.crypt.encrypt(
+            JSON.stringify(mockeData.payloadObject)
+        );
+        await master.pushNewJob(mockeData.payloadObject);
+
+        expect(encryptedPayload).toHaveProperty('iv');
+        expect(encryptedPayload).toHaveProperty('encryptedData');
+        expect(master.jobs).toHaveLength(1);
+        expect(JSON.parse(master.jobs[0])).toEqual(encryptedPayload);
+    });
+    it('Push job succesfully and enrcypted , if job is array', async () => {
+        const encryptedPayload = master.crypt.encrypt(
+            JSON.stringify(mockeData.payloadArray)
+        );
+        await master.pushNewJob(mockeData.payloadArray);
+
+        expect(encryptedPayload).toHaveProperty('iv');
+        expect(encryptedPayload).toHaveProperty('encryptedData');
+        expect(master.jobs).toHaveLength(1);
+        expect(JSON.parse(master.jobs[0])).toEqual(encryptedPayload);
+    });
+    it('Push job succesfully and enrcypted , if job is string', async () => {
+        const jobData = '50';
+        const encryptedPayload = master.crypt.encrypt(jobData);
+        await master.pushNewJob(jobData);
+
+        expect(encryptedPayload).toHaveProperty('iv');
+        expect(encryptedPayload).toHaveProperty('encryptedData');
+        expect(master.jobs).toHaveLength(1);
+        expect(JSON.parse(master.jobs[0])).toEqual(encryptedPayload);
+    });
+    it('Push job succesfully and enrcypted , if job is number', async () => {
+        const jobData = 50;
+        const encryptedPayload = master.crypt.encrypt(jobData);
+        await master.pushNewJob(jobData);
+
+        expect(encryptedPayload).toHaveProperty('iv');
+        expect(encryptedPayload).toHaveProperty('encryptedData');
+        expect(master.jobs).toHaveLength(1);
+        expect(JSON.parse(master.jobs[0])).toEqual(encryptedPayload);
+    });
+});
---
+++ __tests__/utils.js
@@ -0,0 +1,10 @@
+module.exports = {
+    payloadObject: {
+        a: 1,
+        b: 2,
+    },
+    payloadArray: [
+        { a: 1, b: 2 },
+        { a: 10, b: 20 },
+    ],
+};
